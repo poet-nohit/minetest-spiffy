2e07f33f1a6d789b4be6ea32a4bba4bf4aca1dad
poet.nohit <poet.nohit@gmail.com>
2015-03-13 14:49:33 -0600

    updates for def and mobs

--- spiffy/mods/default/nodes.lua
+++ spiffy/mods/default/nodes.lua
@@ -351,7 +351,6 @@ minetest.register_node("default:snow", {
 		end
 	end,
 })
-minetest.register_alias("snow", "default:snow")
 
 minetest.register_node("default:snowblock", {
 	description = "Snow Block",
@@ -700,7 +699,6 @@ minetest.register_node("default:mese", {
 	groups = {cracky=1,level=2},
 	sounds = default.node_sound_stone_defaults(),
 })
-minetest.register_alias("default:mese_block", "default:mese")
 
 
 
